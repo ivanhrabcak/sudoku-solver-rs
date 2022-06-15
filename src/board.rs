dce659834f532d564109fdfb082953dbf957be0f
Ivan Hrabcak <ivan@hrabcak.eu>
2022-06-15 22:46:44 +0200

    Add a non-working solving algorithm

--- src/board.rs
+++ src/board.rs
@@ -5,34 +5,95 @@ pub struct Board {
 }
 
 impl Board {
-    pub fn line_has_number(&self, n: u8, line_ind: usize) -> bool {
+    pub fn count_n_in_line(&self, n: u8, line_ind: usize) -> i32 {
+        let mut counter = 0;
+
         for i in 0..3usize {
             let square = self.board[(line_ind / 3) as usize + i];
             for i in 0..3usize {
                 if square[(line_ind % 3) * 3 + i] == n {
-                    return true;
+                    counter += 1;
                 }
             }
         }
 
-        return false;
+        return counter;
     }
 
-    pub fn column_has_number(&self, n: u8, column_ind: usize) -> bool {
+    pub fn count_n_in_column(&self, n: u8, column_ind: usize) -> i32 {
+        let mut counter = 0;
         for i in 0..3usize {
             let square: [u8; 9] = self.board[(column_ind / 3) as usize + i];
             for j in 0..3usize {
                 if square[3 * j + column_ind % 3] == n {
-                    return true;
+                    counter += 1;
                 }
             }
         }
 
-        return false;
+        return counter;
     }
 
-    pub fn square_has_number(&self, n: u8, square_ind: usize) -> bool {
-        self.board[square_ind].contains(&n)
+    pub fn get_line_for_pos(&self, x: usize, y: usize) -> usize {
+        ((x / 3) * 3) + y / 3
+    }
+
+    pub fn get_column_pos(&self, x: usize, y: usize) -> usize {
+        ((x % 3) * 3) + y % 3 
+    }
+
+    pub fn count_n_in_square(&self, n: u8, square_ind: usize) -> i32 {
+        self.board[square_ind].iter().filter(|x| *x == &n).count() as i32
+    }
+
+    pub fn is_possible(&self, x: usize, y: usize, n: u8) -> bool {
+        return self.count_n_in_square(n, x) == 0
+            && self.count_n_in_line(n, self.get_line_for_pos(x, y)) == 0
+            && self.count_n_in_column(n, self.get_column_pos(x, y)) == 0;
+    }
+
+    pub fn is_solved(&self) -> bool {
+        for (i, line) in self.board.iter().enumerate() {
+            for (j, field) in line.iter().enumerate() {
+                if field == &0 || !self.is_possible(i, j, *field) {
+                    return false;
+                } 
+            }
+        }
+
+        return true;
+    }
+
+    pub fn solve(&mut self) -> Vec<[[u8; 9]; 9]> {
+        println!("{:?}", self.board);
+        if self.is_solved() {
+            return vec![self.board.clone()];
+        }
+
+        let mut solutions: Vec<[[u8; 9]; 9]> = vec![];
+
+        for (i, line) in self.board.clone().iter().enumerate() {
+            for (j, field) in line.iter().enumerate() {
+                if field != &0 {
+                    continue;
+                }
+
+                for possible_n in 1..10 {
+                    if self.is_possible(i, j, possible_n) {
+                        self.board[i][j] = possible_n;
+
+                        solutions.append(&mut self.solve());
+
+                        self.board[i][j] = 0;
+                    }              
+                }
+
+                return vec![];
+            }
+        }
+
+
+        return solutions;
     }
 }
 
