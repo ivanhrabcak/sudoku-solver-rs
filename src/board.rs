548634db255138e8b82e1ea7c1037af4649e0f64
Ivan Hrabcak <ivan@hrabcak.eu>
2022-06-18 14:40:48 +0200

    Improve board printing

--- src/board.rs
+++ src/board.rs
@@ -1,7 +1,11 @@
+use colored::Colorize;
+use terminal_size::terminal_size;
+
 
 #[derive(Debug, Clone, Copy)]
 pub struct Board {
     pub board: [[u8; 9]; 9],
+    terminal_size: (u16, u16)
 }
 
 impl Board {
@@ -49,6 +53,95 @@ impl Board {
         return self.count_n_in_square(n, x) == 0
             && self.count_n_in_line(n, self.get_line_for_pos(x, y)) == 0
             && self.count_n_in_column(n, self.get_column_pos(x, y)) == 0;
+    }
+
+    pub fn is_board_possible(&mut self) -> bool {
+        for (i, square) in self.board.clone().iter().enumerate() {
+            for (j, field) in square.iter().enumerate() {
+                let original_value = *field;
+                self.board[i][j] = 0;
+                if !self.is_possible(i, j, original_value) {
+                    self.board[i][j] = original_value;
+                    return false;
+                }
+
+                self.board[i][j] = original_value;
+            }
+        }
+
+        return true;
+    }
+
+    pub fn print_board(&self, cursor_position: (isize, isize)) {
+        let mut output = String::new();
+        let top_spacing = "\n".repeat(self.terminal_size.1 as usize / 2 - 15 / 2);
+        let padding = " ".repeat(self.terminal_size.0 as usize / 2);
+
+        output += &top_spacing;
+
+        fn color(text: &str, x: usize, y: usize, cursor_position: (isize, isize), board: &Board) -> String {
+            if board.board[x][y] == board.board[cursor_position.0 as usize][cursor_position.1 as usize] {
+                return text.bright_red().to_string();
+            } else if x == cursor_position.0 as usize
+                || board.get_column_pos(x, y) == board.get_column_pos(cursor_position.0 as usize, cursor_position.1 as usize)
+                || board.get_line_for_pos(x, y) == board.get_line_for_pos(cursor_position.0 as usize, cursor_position.1 as usize) {
+                return text.cyan().to_string();
+            } else {
+                return text.to_string();
+            }
+        }
+
+        output += &padding;
+        output += "=========================\n";
+        for line in 0..9usize {
+            for i in 0..3usize {
+                let square = self.board[(line / 3) * 3 as usize + i];
+                if i == 0 {
+                    output += &padding;
+                    output += "|";
+                }
+
+                let x = (line / 3) * 3 as usize + i;
+
+                let mut previous_was_cursor = false;
+                for j in 0..3usize {
+                    let number = square[(line % 3) * 3 + j];
+                    
+                    let y = (line % 3) * 3 + j;
+
+                    if (x as isize, y as isize) == cursor_position {
+                        output += "[";
+                        previous_was_cursor = true;
+                    } else if previous_was_cursor {
+                        output += "]";
+                        previous_was_cursor = false;
+                    } else {
+                        output += " ";
+                    }
+
+                    if number == 0 {
+                        output += &color(" ", x, y, cursor_position, self);
+                    } else {
+                        output += &color(&format!("{}", number), x, y, cursor_position, self);
+                    }
+                }
+
+                if previous_was_cursor {
+                    output += "]";
+                }
+                else {
+                    output += " ";
+                }
+
+                output += "|";
+            }
+            output += "\n";
+            if line % 3 == 2 {
+                output += &padding;
+                output += "=========================\n";
+            }
+        }
+        println!("{}", output);
     }
 
     pub fn is_solved(&mut self) -> bool {
@@ -119,6 +212,12 @@ impl Default for Board {
             }
         }
 
-        return Board{ board };
+        let width = if let Some((w, h)) = terminal_size() {
+                (w.0, h.0)
+            } else {
+                panic!("Failed to get terminal size!");
+            };
+
+        return Board{ board, terminal_size: width };
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,141 @@
 pub mod board;
 
 use board::Board;
+use device_query::{DeviceQuery, DeviceState, Keycode};
+use device_query::Keycode::*;
 
 fn main() {
     let board_array = [[0, 9, 4, 8, 1, 2, 3, 0, 0], [0, 3, 0, 7, 0, 0, 1, 9, 0], [1, 5, 0, 0, 9, 6, 0, 8, 0],
         [0, 3, 0, 0, 0, 8, 0, 0, 6], [9, 0, 4, 6, 1, 3, 2, 0, 0], [6, 7, 0, 0, 4, 9, 0, 3, 1],
         [4, 0, 3, 5, 0, 0, 0, 6, 0], [5, 0, 0, 0, 2, 0, 0, 0, 8], [0, 0, 8, 7, 0, 0, 4, 1, 5]];
-    let mut board = Board { board: board_array };
+    let mut board = Board::default();
+    board.board = board_array;
 
-    println!("{:?}", board.solve());
+    let empty_board = board.clone();
+    let solved_boards = board.clone().solve();
+    
+    let device_state = DeviceState::new();
+    
+    let mut pointer_position: (isize, isize) = (9 / 2, 9 / 2);
+    print!("\x1B[2J\x1B[1;1H");
+    board.print_board(pointer_position);
+
+    let mut previous_keys_held: Vec<Keycode> = vec![];
+    let mut previous_pointer_position = pointer_position;
+    let mut previous_board = board.clone();
+    loop {
+        let keyboard_state = device_state.get_keys();
+        let keys_released: Vec<&Keycode> = keyboard_state.iter().filter(|key| !previous_keys_held.contains(*key)).collect();
+        
+        for key in keys_released {
+            let x = pointer_position.0 as usize;
+            let y = pointer_position.1 as usize;
+            match key {
+                Up => {
+                    if [0, 1, 2].contains(&pointer_position.1) {
+                        if [0, 1, 2].contains(&pointer_position.0) {
+                            continue;
+                        }
+
+                        pointer_position.0 = pointer_position.0 - 3;
+                        pointer_position.1 = pointer_position.1 % 3 + 6;
+                    }
+                    else {
+                        pointer_position.1 -= 3;
+                    }
+                },
+                Down => {
+                    if [6, 7, 8].contains(&pointer_position.1) {
+                        if [6, 7, 8].contains(&pointer_position.0) {
+                            continue;
+                        }
+
+                        pointer_position.0 = pointer_position.0 + 3;
+                        pointer_position.1 = pointer_position.1 % 3;
+                    }
+                    else {
+                        pointer_position.1 += 3;
+                    }
+                },
+                Left => {
+                    if [0, 3, 6].contains(&pointer_position.1) {
+                        if [0, 3, 6].contains(&pointer_position.0) {
+                            continue;
+                        }
+
+                        pointer_position.0 = pointer_position.0 - 1;
+                        pointer_position.1 += 2;
+                    }
+                    else {
+                        pointer_position.1 -= 1;
+                    }
+                },
+                Right => {
+                    if [2, 5, 8].contains(&pointer_position.1) {
+                        if [2, 5, 8].contains(&pointer_position.0) {
+                            continue;
+                        }
+
+                        pointer_position.0 = pointer_position.0 + 1;
+                        pointer_position.1 -= 2;
+                    }
+                    else {
+                        pointer_position.1 += 1;
+                    }
+                },
+                Backspace => board.board[x][y] = 0,
+                Key1 | Numpad1 => board.board[x][y] = 1,
+                Key2 | Numpad2 => board.board[x][y] = 2,
+                Key3 | Numpad3 => board.board[x][y] = 3,
+                Key4 | Numpad4 => board.board[x][y] = 4,
+                Key5 | Numpad5 => board.board[x][y] = 5,
+                Key6 | Numpad6 => board.board[x][y] = 6,
+                Key7 | Numpad7 => board.board[x][y] = 7,
+                Key8 | Numpad8 => board.board[x][y] = 8,
+                Key9 | Numpad9 => board.board[x][y] = 9,
+                _ => ()
+            }
+        }
+
+        if pointer_position.0 < 0 {
+            pointer_position.0 = 0;
+        } else if pointer_position.0 > 8 {
+            pointer_position.0 = 8;
+        }
+
+        if pointer_position.1 < 0 {
+            pointer_position.1 = 0;
+        } else if pointer_position.1 > 8 {
+            pointer_position.1 = 8;
+        }
+
+        
+
+        
+        if previous_pointer_position != pointer_position || previous_board.board != board.board {
+            
+            for (i, square) in empty_board.board.iter().enumerate() {
+                for (j, field) in square.iter().enumerate() {
+                    if field != &0 {
+                        board.board[i][j] = *field;
+                    }
+                }
+            } 
+
+            if solved_boards.contains(&board.board) {
+                break;
+            }
+
+            print!("\x1B[2J\x1B[1;1H");
+            board.print_board(pointer_position);
+        }
+            
+        previous_keys_held = keyboard_state;
+        previous_pointer_position = pointer_position;
+        previous_board = board.clone();
+    }
+
+    println!("You won!");
 }
+
+
